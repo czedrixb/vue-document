71fdbbb4f1099ab5413c418649d705ca541d4a0b
Gavs <81405777+thelastjeremiah@users.noreply.github.com>
2024-02-15 14:27:18 +0800

    utilization of ref & reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    home
+    <!-- uses ref -->
+    <!-- <p ref="p">My name is {{ name }} and my age is {{ age }}</p> -->
+
+    <h1>Home</h1>
+    <h2>Refs</h2>
+    <p>{{ guyOne.name }} - {{ guyOne.age }}</p>
+    <button @click="updateGuyOne">Update Guy One</button>
+    <h2>Reactive</h2>
+    <p>{{ guyTwo.name }} - {{ guyTwo.age }}</p>
+    <button @click="updateGuyTwo">Update Guy Two</button>
   </div>
 </template>
 
 <script>
+import { reactive, ref } from "vue";
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  // always run first, create values, create functions
+  setup() {
+    // ref vs reactive
+    const guyOne = ref({ name: "mario", age: 10 });
+    const guyTwo = reactive({ name: "luigi", age: 15 });
+
+    const updateGuyOne = () => {
+      guyOne.value.age = 20;
+    };
+
+    const updateGuyTwo = () => {
+      guyTwo.age = 25;
+    };
+    return { guyOne, updateGuyOne, guyTwo, updateGuyTwo };
+  },
+};
 </script>
